e864067bb53dc39d16bcd800212041044363a897
John Rusty Lola <johnrustylola@gmail.com>
2024-12-01 12:04:30 +0800

    add day 10 activity complete

--- D10/Activity/script.js
+++ D10/Activity/script.js
@@ -1,26 +1,57 @@
+function Book(){
+    let title = new String();
+    let author = new String();
+    let genre = new String();
+    let availability = new Boolean();
 
-class Book{
-    constructor(title=String,author=String,genre=String,availability=Boolean){
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.availability = availability;
+    this.borrowBook = function(availability) {
+        if (availability)
+            availability = false;
+        
+        console.log(availability);
+        return availability;
     }
 
-    function borrowBook(availability) {
-            
+    this.returnBook = function(isReturn, availability) {
+        if(isReturn)
+            availability = false;
+        console.log(availability);
+        return availability;
     }
 
-    this.returnBook = function() {
-        console.log(title);
-    }
-
-    this.displayInfo = function() {
-        console.log(title);
+    this.displayInfo = function(title, author, genre, availability) {
+        console.log(`Title: ${title} - Author: ${author} - Genre: ${genre} - Availability: ${availability}`);
     }
 }
 
+const book = new Book();
 
-const book = new Book('test');
+book.borrowBook(true);
 
-book.borrowBook();
+function Library(){
+    let books = [];
+    
+    this.addBook = (title, author, genre) => {
+        const Book = {"title":title, "author":author, "genre":genre, "availability": true};
+        books.push(Book);
+    }
+
+    this.searchBook = (title) => {
+        Object.values(books).forEach(value => {
+            if(value.title == title)
+                console.log(`Title: ${value.title} - Author: ${value.author} - Genre: ${value.genre} - Availability: ${value.availability}`);
+        });
+    } 
+
+    this.displayBooks = () => {
+        Object.values(books).forEach(value => {
+            console.log(`Title: ${value.title} - Author: ${value.author} - Genre: ${value.genre} - Availability: ${value.availability}`);
+        });
+    }
+}
+
+const library = new Library();
+library.addBook('test','test','test');
+library.addBook('One Piece','Oda','Adventure');
+library.searchBook('One Piece');
+library.displayBooks();
