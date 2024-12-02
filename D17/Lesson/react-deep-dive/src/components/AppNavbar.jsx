000d234b0f05b2bb6ba14ec05394139b85b489aa
John Rusty Lola <johnrustylola@gmail.com>
2024-12-02 19:02:48 +0800

    add day 17 activity incomplete

--- D17/Lesson/react-deep-dive/src/components/AppNavbar.jsx
+++ D17/Lesson/react-deep-dive/src/components/AppNavbar.jsx
@@ -8,11 +8,13 @@ const AppNavbar = () => {
         <Navbar.Brand href="/">React App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+          <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
-            </Nav>
+            <Nav.Link href="/form">Form</Nav.Link>
+            <Nav.Link href="/product">Product</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
         </Container>
     </Navbar>
